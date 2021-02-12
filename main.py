eb5324d463d6842e33cb444938a2975813daed67
Vaylon Parnell Fernandes <vaylonfernandes@gmail.com>
2021-02-12 15:16:10 +0000

    Added hangman_art.py to import stages and logo and ASCII art. Using https://random-word-api.herokuapp.com/home for words list

---
+++ main.py
@@ -0,0 +1,45 @@
+from random import choice
+from hangman_art import stages, logo
+import requests
+import json 
+
+data=requests.get('https://random-word-api.herokuapp.com/all/?swear=0').text
+words=json.loads(data)
+
+#words= ['aardvak', 'baboon', 'camel']
+chosen_word=choice(words)
+length_of_word=len(chosen_word)
+display = ['_' for _ in range(length_of_word)]
+lives=6
+
+print(logo)
+
+end_of_game=False
+
+while not end_of_game:
+  guess=input('Guess a letter: ').lower()
+
+  if guess in display:
+    print(f'You have already entered {guess}')
+
+  for index, letter in enumerate(chosen_word):
+    if letter==guess:
+      display[index]=guess
+  if guess not in chosen_word:
+    lives-=1
+    print(f'{guess} is not in word. You lost a life')
+    if lives==0:
+      print('You lose..')
+      print(f'The word was {chosen_word}')
+      end_of_game=True
+    
+
+  if '_' not in display:
+    print('You win!!!!!')
+    end_of_game=True
+
+  print(' '.join(display))
+
+  print(stages[lives])
+    
+     
